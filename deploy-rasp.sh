a2a76b7c9c8969fe5c2aab1b77a546572a4142f9
nazori <juaireg@gmail.com>
2020-12-21 23:01:28 +0000

    deploy script updated

--- deploy-rasp.sh
+++ deploy-rasp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "Removing old build"
-rm -r public/
+rm -r public
 rm -r /var/www/btapes/* 
 echo "Publishing webiste"
 hugo
-cp -r public/* /var/www/btapes/*
+cp -r public/* /var/www/btapes
 echo "Website deployed"
+
+
